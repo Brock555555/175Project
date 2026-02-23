9065a9291e05dc1b8fe57aa951bceaa70e4a9220
Notme2222 <nn2wg2@gmail.com>
2026-02-23 02:01:25 -0800

    model is implemented and working, figuring out generation method, currently using chat template

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -16,6 +18,7 @@ from datasets import Dataset
 
 MODEL_NAME = "google/gemma-3-4b-it"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+SAVE_PATH = "./gemma_lora"
 
 fsdp_config = {
     "fsdp_transformer_layer_cls_to_wrap": ["GemmaDecoderLayer"],
@@ -30,12 +33,9 @@ class IdiomaticExpressionModel:
         self.embed_dimensions = embed_dimensions
         self.prefix_length = prefix_length
 
-    def train(self, dataset, batch_size = 16, LR = 2e-5, epochs = 3):
-        print(f"Using device {DEVICE}")
-        dataset = Dataset.from_list(dataset)
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        self.tokenizer.add_special_tokens({"additional_special_tokens": ["[DEF]", "[IDM]"]})
 
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-        # contrastive_encoder = SentenceTransformer('all-MiniLM-L6-v2').to(DEVICE)
 
         gemma_base = AutoModelForCausalLM.from_pretrained(MODEL_NAME).to(DEVICE)
 
@@ -47,11 +47,45 @@ class IdiomaticExpressionModel:
             bias = "none",
             task_type = "CAUSAL_LM"
         )
-        gemma = get_peft_model(gemma_base, lora_config)
+
+        if not os.path.exists(SAVE_PATH):
+            self.is_trained = False
+            print("No saved model found, please call train()")
+            gemma = get_peft_model(gemma_base, lora_config)
+            self.gemma = gemma
+        else:
+            self.is_trained = True
+            gemma = PeftModel.from_pretrained(gemma_base, "./gemma_lora").to(DEVICE)
+            gemma.eval()
+            self.gemma = gemma
+
+    def train(self, dataset, batch_size = 16, LR = 2e-5, epochs = 3, retrain=False):
+        if self.is_trained and not retrain:
+            print("Model already trained. Use retrain=False in train() to force more training or delete the output directory")
+            return
+
+        print(f"Using device {DEVICE}")
+        dataset = Dataset.from_list(dataset)
+
+        def tokenize_function(data):
+            return self.tokenizer(
+                data["text"],
+                padding = "max_length",
+                truncation = True,
+                max_length = 64,
+                return_token_type_ids = True
+            )
+
+        tokenized_dataset = dataset.map(
+            tokenize_function,
+            batched = True,
+            remove_columns = dataset.column_names,
+            load_from_cache_file = False
+        )
 
         trainer = SFTTrainer(
-            model = gemma,
-            train_dataset = dataset,
+            model = self.gemma,
+            train_dataset = tokenized_dataset,
             args=transformers.TrainingArguments(
                 per_device_train_batch_size=batch_size,
                 num_train_epochs = epochs,
@@ -60,32 +94,57 @@ class IdiomaticExpressionModel:
                 max_steps=10,
                 learning_rate=LR,
                 logging_steps = 1,
-                output_dir = "./gemma_lora",
-                optim="paged_adamw_8bit",
+                output_dir = "./gemma_lora_checkpoints",
+                optim="adamw_torch_fused",
                 dataloader_drop_last=True,
                 disable_tqdm = False,
+                remove_unused_columns = False
                 # fsdp = "full_shard",
                 # fsdp_config=fsdp_config
+                # I'll work on implementing FSDP later
             ),
-            data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+            data_collator = DataCollatorForLanguageModeling(self.tokenizer, mlm=False)
         )
 
 
         trainer.train()
+        trainer.save_model("./gemma_lora")
+        self.tokenizer.save_pretrained("./gemma_lora")
+        print("Training Complete")
+        self.is_trained = True
 
 
     def generate_idiom(self, instruction):
-        # load gemma
-        gemma_base = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
-        gemma = PeftModel.from_pretrained(gemma_base, "./gemma_lora").to(DEVICE)
-        gemma.eval()
+        message = [
+            {"role": "system", "content": "Your job is to generate novel idiomatic expressions. No conversational filler or special formatting."},
+            {"role": "user", "content": instruction},
+            {"role": "assistant", "content": "A idiom that fits that definition would be:"}
+        ]
 
+        inputs = self.tokenizer.apply_chat_template(
+            message,
+            tokenize=True,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            return_token_type_ids=True,
+            return_dict = True
+        ).to(DEVICE)
 
-        # load tokenizer
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        # not sure why these are missing, but this does fix the issue
+        if "token_type_ids" not in inputs:
+            inputs["token_type_ids"] = torch.zeros_like(inputs["input_ids"])
 
-        generated = gemma.generate(**instruction)
-        decoded = tokenizer.decode(generated[0]["generated_text"][-1]["content"])
+        with torch.no_grad():
+            outputs = self.gemma.generate(
+                **inputs,
+                max_new_tokens = 64,
+                do_sample=True,
+                temperature=1.1,
+                top_p=0.97,
+                use_cache=True
+            )
 
+        response = outputs[0][inputs["input_ids"].shape[-1]:]
+        decoded = self.tokenizer.decode(response, skip_special_tokens=True)
 
         return decoded
--- model_tester.py
+++ model_tester.py
@@ -3,7 +3,11 @@ from create_dataset import load_urban_dataset, prepare_dataset
 
 if __name__ == "__main__":
     model = IdiomaticExpressionModel()
-    ub_dataset = prepare_dataset(load_urban_dataset()  .sample(frac=0.2))
+    ud_dataset = prepare_dataset(load_urban_dataset().sample(frac=1))
 
-    model.train(ub_dataset)
-    model.generate_idiom("Generate an idiom that means \"to die\"")
+    model.train(ud_dataset)
+
+    prompts = ["to make a mistake", "to die", "to date someone", "to drink alcohol"]
+    for prompt in prompts:
+        output = model.generate_idiom(f"Generate an idiom that means \"{prompt}\"")
+        print(f"For definition {prompt}, the generated idiom was \"{output}\"")
